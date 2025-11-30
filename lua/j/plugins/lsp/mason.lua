f56338d4924d6fd3c5f05a6c67df96b7ec0d3be3
midnightplayer1412 <johnpanda13@gmail.com>
2025-11-30 16:30:00 +0800

    refactor: configure mason file

--- lua/j/plugins/lsp/mason.lua
+++ lua/j/plugins/lsp/mason.lua
@@ -1,145 +1,166 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
-    "hrsh7th/cmp-nvim-lsp",
-    { "folke/neodev.nvim", opts = {} },
-  },
-  event = { "BufReadPre", "BufNewFile" },
-  build = ":MasonUpdate",
-  config = function()
-    -- 1. Setup completion capabilities
-    local cmp_nvim_lsp = require("cmp_nvim_lsp")
-    local capabilities = cmp_nvim_lsp.default_capabilities()
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
+		"hrsh7th/cmp-nvim-lsp",
+		{ "folke/neodev.nvim", opts = {} },
+	},
+	event = { "BufReadPre", "BufNewFile" },
+	build = ":MasonUpdate",
+	config = function()
+		-- 1. Setup completion capabilities
+		local cmp_nvim_lsp = require("cmp_nvim_lsp")
+		local capabilities = cmp_nvim_lsp.default_capabilities()
 
-    -- 2. Configure LSP servers using vim.lsp.config() (new API)
-    -- Lua
-    vim.lsp.config("lua_ls", {
-      capabilities = capabilities,
-      settings = {
-        Lua = {
-          diagnostics = { globals = { "vim" } },
-          workspace = {
-            library = vim.api.nvim_get_runtime_file("", true),
-            checkThirdParty = false,
-          },
-        },
-      },
-    })
+		-- 2. Configure LSP servers using vim.lsp.config() (new API)
+		-- Lua
+		vim.lsp.config("lua_ls", {
+			capabilities = capabilities,
+			settings = {
+				Lua = {
+					diagnostics = { globals = { "vim" } },
+					workspace = {
+						library = vim.api.nvim_get_runtime_file("", true),
+						checkThirdParty = false,
+					},
+				},
+			},
+		})
 
-    -- PHP / Laravel
-    vim.lsp.config("intelephense", {
-      capabilities = capabilities,
-      settings = {
-        intelephense = {
-          files = {
-            associations = { "*.php", "*.blade.php" },
-          },
-          environment = {
-            includePaths = { "vendor" },
-          },
-        },
-      },
-    })
+		-- PHP / Laravel
+		vim.lsp.config("intelephense", {
+			capabilities = capabilities,
+			settings = {
+				intelephense = {
+					files = {
+						associations = { "*.php", "*.blade.php" },
+					},
+					environment = {
+						includePaths = { "vendor" },
+					},
+				},
+			},
+		})
 
-    -- HTML / Blade
-    vim.lsp.config("html", {
-      capabilities = capabilities,
-      filetypes = { "html", "blade" },
-    })
+		-- HTML / Blade
+		vim.lsp.config("html", {
+			capabilities = capabilities,
+			filetypes = { "html", "blade" },
+		})
 
-    -- Emmet for Blade & HTML
-    vim.lsp.config("emmet_ls", {
-      capabilities = capabilities,
-      filetypes = {
-        "html",
-        "blade",
-        "css",
-        "javascriptreact",
-        "typescriptreact",
-        "svelte",
-      },
-    })
+		-- Emmet for Blade & HTML
+		vim.lsp.config("emmet_ls", {
+			capabilities = capabilities,
+			filetypes = {
+				"html",
+				"blade",
+				"css",
+				"javascriptreact",
+				"typescriptreact",
+				"svelte",
+			},
+		})
 
-    -- CSS
-    vim.lsp.config("cssls", {
-      capabilities = capabilities,
-    })
+		-- CSS
+		vim.lsp.config("cssls", {
+			capabilities = capabilities,
+		})
 
-    -- TypeScript
-    vim.lsp.config("ts_ls", {
-      capabilities = capabilities,
-    })
+		-- TypeScript
+		vim.lsp.config("ts_ls", {
+			capabilities = capabilities,
+		})
 
-    -- Python
-    vim.lsp.config("pyright", {
-      capabilities = capabilities,
-    })
+		-- Python
+		vim.lsp.config("pyright", {
+			capabilities = capabilities,
+		})
 
-    -- C/C++
-    vim.lsp.config("clangd", {
-      capabilities = capabilities,
-    })
+		-- C/C++
+		vim.lsp.config("clangd", {
+			capabilities = capabilities,
+		})
 
-    -- 3. Setup mason
-    local mason = require("mason")
-    mason.setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        },
-      },
-    })
+		-- 3. Setup mason
+		local mason = require("mason")
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
 
-    -- 4. Setup mason-lspconfig (with automatic_enable)
-    local has_mason_lspconfig, mason_lspconfig = pcall(require, "mason-lspconfig")
-    if not has_mason_lspconfig then
-      vim.notify("mason-lspconfig not found! Please run :Lazy sync", vim.log.levels.ERROR)
-      return
-    end
+		-- 4. Setup mason-lspconfig (with automatic_enable)
+		local has_mason_lspconfig, mason_lspconfig = pcall(require, "mason-lspconfig")
+		if not has_mason_lspconfig then
+			vim.notify("mason-lspconfig not found! Please run :Lazy sync", vim.log.levels.ERROR)
+			return
+		end
 
-    mason_lspconfig.setup({
-      ensure_installed = {
-        "intelephense",
-        "html",
-        "cssls",
-        "emmet_ls",
-        "lua_ls",
-        "ts_ls",
-        "pyright",
-        "clangd",
-      },
-      automatic_enable = true, -- Automatically enable installed servers
-    })
+		mason_lspconfig.setup({
+			ensure_installed = {
+				"intelephense",
+				"html",
+				"cssls",
+				"emmet_ls",
+				"lua_ls",
+				"ts_ls",
+				"pyright",
+				"clangd",
+			},
+			automatic_enable = true, -- Automatically enable installed servers
+		})
 
-    -- 5. Diagnostic icons
-    local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-    for type, icon in pairs(signs) do
-      local hl = "DiagnosticSign" .. type
-      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-    end
+		-- 5. Diagnostic icons
+		local signs = { Error = "", Warn = "", Hint = "󰠠", Info = "" }
+		vim.diagnostic.config({
+			virtual_text = {
+				prefix = "●", -- could be '■', '▎', '●', etc.
+				spacing = 2,
+			},
+			signs = true, -- show the sign column icons
+			underline = true, -- underline problematic code
+			update_in_insert = false, -- don't update diagnostics while typing
+			severity_sort = true, -- sort by severity
+			float = {
+				border = "rounded", -- border style for floating window
+				source = "always", -- show source (LSP server name) in float
+				header = "",
+				prefix = "",
+			},
+		})
 
-    -- 6. Keymaps when LSP attaches
-    vim.api.nvim_create_autocmd("LspAttach", {
-      group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-      callback = function(ev)
-        local opts = { buffer = ev.buf, silent = true }
-        local keymap = vim.keymap
+		for type, icon in pairs(signs) do
+			local hl = "DiagnosticSign" .. type
+			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+		end
 
-        keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
-        keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-        keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
-        keymap.set("n", "gr", "<cmd>Telescope lsp_references<CR>", opts)
-        keymap.set("n", "K", vim.lsp.buf.hover, opts)
-        keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-        keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-        keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
-        keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
-        keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
-      end,
-    })
-  end,
+		-- 6. Keymaps when LSP attaches
+		vim.api.nvim_create_autocmd("LspAttach", {
+			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+			callback = function(ev)
+				local opts = { buffer = ev.buf, silent = true }
+				local keymap = vim.keymap
+
+				keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
+				keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+				keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
+				keymap.set("n", "gr", "<cmd>Telescope lsp_references<CR>", opts)
+				keymap.set("n", "K", vim.lsp.buf.hover, opts)
+				keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+				keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+				keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+				keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+				keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+				keymap.set("n", "<leader>q", vim.diagnostic.setloclist, opts) -- send all diagnostics to location list
+				keymap.set("n", "<leader>gg", function()
+					require("telescope.builtin").diagnostics({ bufnr = 0 }) -- current buffer
+				end, opts)
+			end,
+		})
+	end,
 }
